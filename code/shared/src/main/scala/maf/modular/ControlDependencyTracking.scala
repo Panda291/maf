9d44d962e120b1ea210dce138d91bc3f3d5113bd
christophe <christophevandeneede@hotmail.com>
2023-04-16 11:05:10 +0200

    dependency tracking implemented

--- code/shared/src/main/scala/maf/modular/ControlDependencyTracking.scala
+++ code/shared/src/main/scala/maf/modular/ControlDependencyTracking.scala
@@ -7,31 +7,24 @@ import maf.util.benchmarks.Timeout
 import maf.modular.worklist.FIFOWorklistAlgorithm
 import maf.modular.scheme.*
 import maf.language.scheme.SchemeParser
+import scala.collection.mutable
 
 
 trait ControlDependencyTracking extends DependencyTracking[SchemeExp] with BigStepModFSemantics {
-  var condDependencies: Map[Identity, Set[Identity]] = Map().withDefaultValue(Set.empty)
+  var condDependencies: mutable.Map[Identity, Set[Identity]] = mutable.Map().withDefaultValue(Set.empty)
   var debugExpressionSet: Set[SchemeExp] = Set.empty
 
   override def intraAnalysis(component: Component): ControlDependencyTrackingIntra
 
   trait ControlDependencyTrackingIntra extends BigStepModFIntra with DependencyTrackingIntra:
-//        abstract override def analyzeWithTimeout(timeout: Timeout.T): Unit = {
-//          expr(component) match
-//    //        case SchemeIf(cond, cons, alt, idn) => { debugExpressionSet += cond
-//    //                                                 debugExpressionSet += cons
-//    //                                                 debugExpressionSet += alt }
-//            case SchemeIf(_,_,_,_) => debugExpressionSet += expr(component).asInstanceOf[SchemeExp]
-//            case _ => debugExpressionSet += expr(component).asInstanceOf[SchemeExp]
-//          super.analyzeWithTimeout(timeout)
-//        }
-
     override def eval(exp: SchemeExp): EvalM[Value] = {
           exp match
             case SchemeIf(cond, cons, alt, idn) if alt.idn == exp.idn =>
               condDependencies = condDependencies.++(Map(cond.idn -> Set(cons.idn)))
+              condDependencies(cond.idn) += cons.idn
             case SchemeIf(cond, cons, alt, idn) => // alt exists
-              condDependencies = condDependencies.++(Map(cond.idn -> Set(cons.idn, alt.idn)))
+              condDependencies(cond.idn) += cons.idn
+              condDependencies(cond.idn) += alt.idn
             case _ =>
           super.eval(exp)
         }
